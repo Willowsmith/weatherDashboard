bccd26b62409dcc54b1adf77e0ef1e65a971c6c6
Willowsmith <willowrosedennis@gmail.com>
2021-10-22 10:35:23 -0700

    Finished the logic and css, organized my html.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # weatherDashboard
+
+
+## Creating a clean and responsive website for a professional portfolio.
+
+-My motivation was to create code to allow a user to check weather in multiple cities and see not only their search history but also the weather for the next 5 days.
+-I built this project so that I could learn how to create a dynamic website that would pull information via APIs and display it.
+-The problem that I solved were the display of data searched for in a separate website in a clean and responsive manner.
+-I learned about APIs and how to display them properly.
+
+# Installation
+
+There are no steps required to install this project, simply launch the website in your preferred web browser and select the generate password button. You can locate the website at: [Website:](https://github.com/Willowsmith/weatherDashboard)
+
+# Usage
+
+Launch the website and utilize the functions found therein.
+
+# Credits
+
+Melyssa Dennis
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,134 @@
-var searchFormEl = document.querySelector('#search-form');
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
-
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+var searchCity = document.querySelector('#searchCity');
+var cityInput = document.querySelector('#searchBar');
+var cityForecast = document.querySelector('#city-forecast');
 
 
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split('&');
+//Todays Variables
+var cityName = document.querySelector('#city-name');
+var day0 = document.querySelector('#day0');
+var icon0 = document.querySelector('#icon0');
+var temp0 = document.querySelector('#temp0');
+var wind0 = document.querySelector('#wind0');
+var humid0 = document.querySelector('#humid0');
+var uv0 = document.querySelector('#uv0');
 
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-  var format = searchParamsArr[1].split('=').pop();
+//Tomorrows Variables
+var day1 = document.querySelector('#day1');
+var icon1 = document.querySelector('#icon1');
+var temp1 = document.querySelector('#temp1');
+var wind1 = document.querySelector('#wind1');
+var humid1 = document.querySelector('#humid1');
 
-  searchApi(query, format);
-}
 
-function printResults(resultObj) {
-  console.log(resultObj);
+//Day 2 variables
+var day2 = document.querySelector('#day2');
+var icon2 = document.querySelector('#icon2');
+var temp2 = document.querySelector('#temp2');
+var wind2 = document.querySelector('#wind2');
+var humid2 = document.querySelector('#humid2');
 
-  // set up `<div>` to hold result content
-  var resultCard = document.createElement('div');
-  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
-  resultCard.append(resultBody);
+//Day 3 variables
+var day3 = document.querySelector('#day3');
+var icon3 = document.querySelector('#icon3');
+var temp3 = document.querySelector('#temp3');
+var wind3 = document.querySelector('#wind3');
+var humid3 = document.querySelector('#humid3');
 
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
 
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
+//Day 4 variables
+var day4 = document.querySelector('#day4');
+var icon4 = document.querySelector('#icon4');
+var temp4 = document.querySelector('#temp4');
+var wind4 = document.querySelector('#wind4');
+var humid4 = document.querySelector('#humid4');
 
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
 
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
+//Day 5 variables
+var day5 = document.querySelector('#day5');
+var icon5 = document.querySelector('#icon5');
+var temp5 = document.querySelector('#temp5');
+var wind5 = document.querySelector('#wind5');
+var humid5 = document.querySelector('#humid5');
 
-  var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
+var city;  //Input from user
+cityForecast.setAttribute("style", "display: none")
 
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+var citySubmitHandler = function (event) {
+    event.preventDefault();
+  
+    city = cityInput.value.trim();
+    console.log(city)
+    if (city) {
+        getCityInfo(city);
+        cityForecast.setAttribute("style", "display: ")
+    } else {
+        alert('Please enter a city');
+    }
+  };
 
-  resultContentEl.append(resultCard);
-}
+var APIkey = "b1048ffe692830f2dafbaad135f0fd00";
 
-function searchApi(query, format) {
-  var locQueryUrl = 'https://openweathermap.org/api/one-call-api/?fo=json';
 
-  if (format) {
-    locQueryUrl = 'https://openweathermap.org/api/one-call-api/' + format + '/?fo=json';
-  }
 
-  locQueryUrl = locQueryUrl + '&q=' + query;
 
-  fetch(locQueryUrl)
+function getCityInfo() {
+    var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    fetch(queryURL1)
     .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-
-      return response.json();
+        return response.json();
     })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
+    .then(function (data) {
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
+        
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + APIkey;
+            fetch(queryURL2)  
+                .then(function (response) {
+                    return response.json();
+                }) 
+                .then(function (data) {
+                    
+                    cityName.textContent = city.charAt(0).toUpperCase() + city.slice(1);
+                    day0.textContent = moment.unix(data.current.dt).format("MM-DD-YY");
 
-      console.log(locRes);
+                    temp0.textContent = "Temperature: " + data.current.temp + " Deg F";
+                    wind0.textContent = "Wind-Speed: " + data.current.wind_speed + "mph";
+                    humid0.textContent = "Humidity: " + data.current.humidity + "%";
+                    uv0.textContent = "UV Index: " + data.current.uvi;
 
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}
+                    day1.textContent = moment.unix(data.daily[1].dt).format("MM-DD-YY");
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
+                    temp1.textContent = "Temp: " + data.daily[1].temp.day + "F";
+                    wind1.textContent = "Wind: " + data.daily[1].wind_speed + "mph";
+                    humid1.textContent = "Humidity: " + data.daily[1].humidity + "%";
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
+                    day2.textContent = moment.unix(data.daily[2].dt).format("MM-DD-YY");
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
+                    temp2.textContent = "Temp: " + data.daily[2].temp.day + "F";
+                    wind2.textContent = "Wind: " + data.daily[2].wind_speed + "mph";
+                    humid2.textContent = "Humidity: " + data.daily[2].humidity + "%";
 
-  searchApi(searchInputVal, formatInputVal);
-}
+                    day3.textContent = moment.unix(data.daily[3].dt).format("MM-DD-YY");
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+                    temp3.textContent = "Temp: " + data.daily[3].temp.day + "F";
+                    wind3.textContent = "Wind: " + data.daily[3].wind_speed + "mph";
+                    humid3.textContent = "Humidity: " + data.daily[3].humidity + "%";
 
-getParams();
+                    day4.textContent = moment.unix(data.daily[4].dt).format("MM-DD-YY");
+
+                    temp4.textContent = "Temp: " + data.daily[4].temp.day + "F";
+                    wind4.textContent = "Wind: " + data.daily[4].wind_speed + "mph";
+                    humid4.textContent = "Humidity: " + data.daily[4].humidity + "%";
+
+                    day5.textContent = moment.unix(data.daily[5].dt).format("MM-DD-YY");
+
+                    temp5.textContent = "Temp: " + data.daily[5].temp.day + "F";
+                    wind5.textContent = "Wind: " + data.daily[5].wind_speed + "mph";
+                    humid5.textContent = "Humidity: " + data.daily[5].humidity + "%";
+                })
+
+        })
+    }
+
+searchCity.addEventListener('submit', citySubmitHandler);
